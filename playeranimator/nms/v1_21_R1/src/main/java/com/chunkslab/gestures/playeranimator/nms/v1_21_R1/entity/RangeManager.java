4ebef36d44ff4d1e71bbd87eb798952f368c0a48
Emre <amowny08@gmail.com>
2025-04-25 11:48:07 +0300

    update 1.21.1 playeranimator

--- playeranimator/nms/v1_21_R1/src/main/java/com/chunkslab/gestures/playeranimator/nms/v1_21_R1/entity/RangeManager.java
+++ playeranimator/nms/v1_21_R1/src/main/java/com/chunkslab/gestures/playeranimator/nms/v1_21_R1/entity/RangeManager.java
@@ -1,45 +1,109 @@
 package com.chunkslab.gestures.playeranimator.nms.v1_21_R1.entity;
 
 import com.chunkslab.gestures.playeranimator.api.nms.IRangeManager;
-import com.chunkslab.gestures.playeranimator.api.utils.FieldUtils;
-import net.minecraft.server.level.ChunkMap;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerPlayerConnection;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
 public class RangeManager implements IRangeManager {
 
-    private final ChunkMap.TrackedEntity tracked;
+    private final Object tracked; // Use Object instead of ChunkMap.TrackedEntity for full reflection
 
-    public RangeManager(ChunkMap.TrackedEntity tracked) {
+    public RangeManager(Object tracked) {
         this.tracked = tracked;
     }
 
     @Override
     public void addPlayer(Player player) {
-        tracked.seenBy.add(((CraftPlayer) player).getHandle().connection);
+        try {
+            // Get the 'seenBy' field from the TrackedEntity class
+            Field seenByField = getField(tracked.getClass(), "seenBy");
+            seenByField.setAccessible(true);
+
+            // Get the 'seenBy' Set from the tracked entity
+            Set<ServerPlayerConnection> seenBy = (Set<ServerPlayerConnection>) seenByField.get(tracked);
+
+            // Get the NMS player connection from the CraftPlayer
+            ServerPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+
+            // Add the player connection to the 'seenBy' Set
+            seenBy.add(nmsPlayer.connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removePlayer(Player player) {
-        tracked.seenBy.remove(((CraftPlayer) player).getHandle().connection);
+        try {
+            // Get the 'seenBy' field from the TrackedEntity class
+            Field seenByField = getField(tracked.getClass(), "seenBy");
+            seenByField.setAccessible(true);
+
+            // Get the 'seenBy' Set from the tracked entity
+            Set<ServerPlayerConnection> seenBy = (Set<ServerPlayerConnection>) seenByField.get(tracked);
+
+            // Get the NMS player connection from the CraftPlayer
+            ServerPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+
+            // Add the player connection to the 'seenBy' Set
+            seenBy.remove(nmsPlayer.connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void setRenderDistance(int radius) {
         try {
-            FieldUtils.getField(ChunkMap.TrackedEntity.class, "d").setInt(tracked, radius);
-        } catch (IllegalAccessException e) {
+            // Get the 'd' field (render distance) from the TrackedEntity class
+            Field renderDistanceField = getField(tracked.getClass(), "d");
+            renderDistanceField.setAccessible(true);
+
+            // Set the render distance value
+            renderDistanceField.setInt(tracked, radius);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Override
     public Set<Player> getPlayerInRange() {
-        Set<Player> list = new HashSet<>();
-        tracked.seenBy.forEach(serverPlayerConnection -> list.add(serverPlayerConnection.getPlayer().getBukkitEntity()));
-        return list;
+        Set<Player> players = new HashSet<>();
+        try {
+            // Get the 'seenBy' field from the TrackedEntity class
+            Field seenByField = getField(tracked.getClass(), "seenBy");
+            seenByField.setAccessible(true);
+
+            // Get the 'seenBy' Set from the tracked entity
+            Set<?> seenBy = (Set<?>) seenByField.get(tracked);
+
+            // Iterate over the 'seenBy' Set and convert NMS players to Bukkit players
+            for (Object playerConnection : seenBy) {
+                Method getPlayerMethod = playerConnection.getClass().getMethod("getPlayer");
+                Object nmsPlayer = getPlayerMethod.invoke(playerConnection);
+
+                Method getBukkitEntityMethod = nmsPlayer.getClass().getMethod("getBukkitEntity");
+                Player bukkitPlayer = (Player) getBukkitEntityMethod.invoke(nmsPlayer);
+
+                players.add(bukkitPlayer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return players;
+    }
+
+    // Helper method to get a field from a class (including private fields)
+    private Field getField(Class<?> clazz, String fieldName) throws NoSuchFieldException {
+        Field field = clazz.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field;
     }
 }
--- playeranimator/nms/v1_21_R1/src/main/java/com/chunkslab/gestures/playeranimator/nms/v1_21_R1/entity/RendererImpl.java
+++ playeranimator/nms/v1_21_R1/src/main/java/com/chunkslab/gestures/playeranimator/nms/v1_21_R1/entity/RendererImpl.java
@@ -6,7 +6,9 @@ import com.chunkslab.gestures.playeranimator.api.model.player.bones.PlayerBone;
 import com.chunkslab.gestures.playeranimator.api.nms.IRenderer;
 import com.chunkslab.gestures.playeranimator.api.texture.TextureWrapper;
 import com.mojang.datafixers.util.Pair;
+import io.netty.buffer.Unpooled;
 import net.minecraft.core.Rotations;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
@@ -31,6 +33,7 @@ import java.util.List;
 public class RendererImpl implements IRenderer {
 
     private final List<Pair<EquipmentSlot, ItemStack>> equipments = new ArrayList<>();
+    private final List<Pair<EquipmentSlot, ItemStack>> invisibleEquipments = new ArrayList<>();
     private ArmorStand armorStand;
     private AreaEffectCloud cloud;
 
@@ -71,9 +74,14 @@ public class RendererImpl implements IRenderer {
             }
             org.bukkit.inventory.ItemStack playerHead = PlayerAnimator.api.getNms().setSkullTexture(null, textureWrapper);
             ItemMeta meta = playerHead.getItemMeta();
-            meta.setCustomModelData(LimbType.INVISIBLE_HEAD.getModelId());
+            meta.setCustomModelData(textureWrapper.isSlim() ? limb.getType().getSlimId() : limb.getType().getModelId());
             playerHead.setItemMeta(meta);
             equipments.add(Pair.of(EquipmentSlot.MAINHAND, CraftItemStack.asNMSCopy(playerHead)));
+            org.bukkit.inventory.ItemStack invisiblePlayerHead = PlayerAnimator.api.getNms().setSkullTexture(null, textureWrapper);
+            meta = invisiblePlayerHead.getItemMeta();
+            meta.setCustomModelData(LimbType.INVISIBLE_HEAD.getModelId());
+            invisiblePlayerHead.setItemMeta(meta);
+            invisibleEquipments.add(Pair.of(EquipmentSlot.MAINHAND, CraftItemStack.asNMSCopy(playerHead)));
         }
     }
 
@@ -123,11 +131,11 @@ public class RendererImpl implements IRenderer {
         cloud.setPos(finalLocation.getX(), finalLocation.getY(), finalLocation.getZ());
         Vec3 ridingPosition = this.armorStand.getPassengerRidingPosition(cloud);
         Vec3 vehicleAttachmentPoint = this.cloud.getVehicleAttachmentPoint(armorStand);
-        armorStand.moveTo(finalLocation.getX(), finalLocation.getY() + (ridingPosition.y - vehicleAttachmentPoint.y), finalLocation.getZ(), limb.getModel().getBaseYaw(), 0);
+        armorStand.moveTo(finalLocation.getX(), finalLocation.getY() - 0.2, finalLocation.getZ(), limb.getModel().getBaseYaw(), 0);
 
         ClientboundAddEntityPacket asSpawn = new ClientboundAddEntityPacket(armorStand, 0, armorStand.blockPosition());
         ClientboundSetEntityDataPacket asMeta = new ClientboundSetEntityDataPacket(armorStand.getId(), armorStand.getEntityData().getNonDefaultValues());
-        ClientboundSetEquipmentPacket asEquip = new ClientboundSetEquipmentPacket(armorStand.getId(), equipments);
+        ClientboundSetEquipmentPacket asEquip = limb.isInvisible() && limb.getType().getModelId() != -1 ? new ClientboundSetEquipmentPacket(armorStand.getId(), invisibleEquipments) : new ClientboundSetEquipmentPacket(armorStand.getId(), equipments);
         ClientboundAddEntityPacket aecSpawn = new ClientboundAddEntityPacket(cloud, 0, cloud.blockPosition());
         ClientboundSetEntityDataPacket aecMeta = new ClientboundSetEntityDataPacket(cloud.getId(), cloud.getEntityData().getNonDefaultValues());
         ClientboundSetPassengersPacket mount = new ClientboundSetPassengersPacket(cloud);
@@ -150,12 +158,20 @@ public class RendererImpl implements IRenderer {
         else
             armorStand.setRightArmPose(toNMS(finalRotation));
 
+        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+        buf.writeVarInt(cloud.getId());
+        buf.writeDouble(armorStand.getX());
+        buf.writeDouble(armorStand.getY());
+        buf.writeDouble(armorStand.getZ());
+        buf.writeByte((byte) armorStand.getBukkitYaw());
+        buf.writeByte((byte) armorStand.getXRot());
+        buf.writeBoolean(false);
+        ClientboundTeleportEntityPacket teleport = ClientboundTeleportEntityPacket.STREAM_CODEC.decode(buf);
 
-        ClientboundTeleportEntityPacket teleport = new ClientboundTeleportEntityPacket(cloud);
-        ClientboundMoveEntityPacket.Rot rotate = new ClientboundMoveEntityPacket.Rot(armorStand.getId(), IRenderer.rotByte(limb.getModel().getBaseYaw()), (byte) 0, false);
+        //ClientboundMoveEntityPacket.Rot rotate = new ClientboundMoveEntityPacket.Rot(armorStand.getId(), IRenderer.rotByte(limb.getModel().getBaseYaw()), (byte) 0, false);
         ClientboundSetEntityDataPacket meta = new ClientboundSetEntityDataPacket(armorStand.getId(), armorStand.getEntityData().getNonDefaultValues());
-
-        return Arrays.asList(teleport, rotate, meta);
+        ClientboundSetEquipmentPacket asEquip = limb.isInvisible() && limb.getType().getModelId() != -1 ? new ClientboundSetEquipmentPacket(armorStand.getId(), invisibleEquipments) : new ClientboundSetEquipmentPacket(armorStand.getId(), equipments);
+        return Arrays.asList(asEquip, teleport, meta);
     }
 
     private Rotations toNMS(EulerAngle angle) {
